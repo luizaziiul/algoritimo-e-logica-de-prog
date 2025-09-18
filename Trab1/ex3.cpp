46a313fd8cb0e0ce882ac8e1872819ba65185f04
luizaziiul <luizaaraujo.souza@outlook.com>
2025-09-18 20:35:57 -0300

    Create ex3.cpp

---
+++ Trab1/ex3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n1,n2;
+    
+    cout << "escreva dois numeros: ";
+    cin >> n1 >> n2;
+    
+    if (n1 < n2){
+        cout << n1 << " menor que " << n2;
+    }else if (n2 < n1){
+        cout << n2 << " menor que " << n1;
+    }
+    
+	return 0;
+}
