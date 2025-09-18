fa3dce638b5fd7dd2c5d376935de3fa1d5da2cc6
luizaziiul <luizaaraujo.souza@outlook.com>
2025-09-18 20:17:16 -0300

    Update ex2.cpp

--- Trab1/ex2.cpp
+++ Trab1/ex2.cpp
@@ -4,19 +4,28 @@ using namespace std;
 
 int main() {
     
-    int n1, n2, n3, media;
+    float n1, n2, media;
+    string msg;
     
-    cout << "informe suas notas:";
-    cin >> n1 >> n2 >> n3;
     
-    media = (n1+n2+n3)/3
+    cout << "informe suas 2 notas:";
+    cin >> n1 >> n2;
+    
+    media = (n1+n2)/2;
     
     if (media >= 0 and media <3){
-        cout << "reprovado";
-    }else if (media >=3 and media < 7){
-        
+        msg =  "Foi reprovado";
+    }else{
+        if (media >=3 and media < 7){
+            msg = " Ficou de exame";
+            
+        }else {
+            msg =  "Foi aprovado";
+        }
+        cout << "Sua media eh: " << media << "\nStatus: " << msg << endl;
     }
     
 
 	return 0;
+}
 }
