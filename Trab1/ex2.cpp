e62cda19012fdeb05d60be0538f32985feffc473
luizaziiul <luizaaraujo.souza@outlook.com>
2025-09-17 21:11:45 -0300

    aaa

---
+++ Trab1/ex2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n1, n2, n3, media;
+    
+    cout << "informe suas notas:";
+    cin >> n1 >> n2 >> n3;
+    
+    media = (n1+n2+n3)/3
+    
+    if (media >= 0 and media <3){
+        cout << "reprovado";
+    }else if (media >=3 and media < 7){
+        
+    }
+    
+
+	return 0;
+}
